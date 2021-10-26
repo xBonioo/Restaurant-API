25ee475532054723c7fb4155e40c24d562b844e2
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2021-10-26 11:08:43 +0200

    Entity framework added

--- RestaurantApi/Startup.cs
+++ RestaurantApi/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestaurantCommon.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,8 +28,9 @@ namespace RestaurantApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddDbContext<RestaurantDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Task")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestaurantApi", Version = "v1" });
