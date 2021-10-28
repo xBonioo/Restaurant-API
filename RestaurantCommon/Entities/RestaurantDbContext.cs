2c9be7a587a50ce5243f9d8bd1e8e23fe1c540b9
Kamil Świderski <swiderski.kamil.1998@gmail.com>
2021-10-28 09:51:44 +0200

    small fixes, add User and Role

--- RestaurantCommon/Entities/RestaurantDbContext.cs
+++ RestaurantCommon/Entities/RestaurantDbContext.cs
@@ -12,6 +12,8 @@ namespace RestaurantCommon.Entities
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Dish> Dishes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public RestaurantDbContext(DbContextOptions options) : base(options)
         {
@@ -25,6 +27,24 @@ namespace RestaurantCommon.Entities
                 .HasMaxLength(32);
 
             modelBuilder.Entity<Dish>()
+                .Property(r => r.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Address>()
+                .Property(r => r.City)
+                .IsRequired()
+                .HasMaxLength(48);
+
+            modelBuilder.Entity<Address>()
+                .Property(r => r.Street)
+                .IsRequired()
+                .HasMaxLength(48);
+
+            modelBuilder.Entity<User>()
+                .Property(r => r.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
                 .Property(r => r.Name)
                 .IsRequired();
         }
--- RestaurantCommon/Migrations/20211028074211_NewTable.Designer.cs
+++ RestaurantCommon/Migrations/20211028074211_NewTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantCommon.Entities;
 
 namespace RestaurantCommon.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211028074211_NewTable")]
+    partial class NewTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
